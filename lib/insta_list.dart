98215af5965003f597788627f1d8f430bb4d495f
Terrion Morson <terrionmorson821@gmail.com>
2019-04-18 11:23:57 -0500

    Updated body

---
+++ lib/insta_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InstaList extends    StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+    itemCount:5,
+    itemBuilder: (context, index) => new Stack(),
+    );
+  }
+}
